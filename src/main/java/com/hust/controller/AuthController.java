e4900cfb629e553b14fb6c821aadbaaa672e2fa1
baidu <baidu@localhost>
2016-08-07 18:32:04 +0800

    权限控制

---
+++ src/main/java/com/hust/controller/AuthController.java
@@ -0,0 +1,14 @@
+package com.hust.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AuthController {
+	
+	
+	public ModelAndView login(String userName, String passwd) {
+		
+		return null;
+	}
+}
