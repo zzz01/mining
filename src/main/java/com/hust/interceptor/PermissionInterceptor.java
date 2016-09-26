4d0af5a513d2402e7b1f2c358d2dc438ea9ddbae
gaoyan10 <gaoyan10@baidu.com>
2016-09-26 13:28:05 +0800

    增加权限不足提示

--- src/main/java/com/hust/interceptor/PermissionInterceptor.java
+++ src/main/java/com/hust/interceptor/PermissionInterceptor.java
@@ -6,15 +6,22 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import com.hust.util.ResultUtil;
 
 public class PermissionInterceptor implements HandlerInterceptor {
     /**
      * Logger for this class
      */
     private static final Logger LOG = LoggerFactory.getLogger(PermissionInterceptor.class);
+
+    @Autowired
+    private MappingJackson2HttpMessageConverter converter;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
@@ -34,12 +41,12 @@ public class PermissionInterceptor implements HandlerInterceptor {
                 } else {
                     LOG.warn("PermissionDeny: errorMsg=用户{}没有权限，访问的URL：{}", request.getRemoteHost(),
                             request.getRequestURI());
-                    response.sendRedirect("/index.html");
+                    fail(response);
                 }
             }
         } catch (Exception e) {
             // TODO Auto-generated catch block
-            LOG.error("redirect to index.html error \t" + e.toString());
+            LOG.error("permissionInterceptor error \t" + e.toString());
             try {
                 response.sendRedirect("/page/error.html");
             } catch (Exception e1) {
@@ -61,6 +68,17 @@ public class PermissionInterceptor implements HandlerInterceptor {
             throws Exception {
         // TODO Auto-generated method stub
 
+    }
+
+    private void fail(HttpServletResponse response) {
+        try {
+            response.setCharacterEncoding("UTF-8");
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            converter.write(ResultUtil.errorWithMsg("非常抱歉，您没有权限访问该资源，请联系管理员"), MediaType.APPLICATION_JSON,
+                    new ServletServerHttpResponse(response));
+        } catch (Throwable e) {
+            LOG.error("ajax error \t" + e);
+        }
     }
 
 }
